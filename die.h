577b07023c77ff1dd45eb692f56cf7ef9baefd01
Chris Kastorff <encryptio@gmail.com>
2009-07-10 10:52:37 -0400

    license most of the code with the GPL, die.h with BSD 3-clause
    as required by fftw's GPL license. bullshit, i tell ya.

--- die.h
+++ die.h
@@ -1,4 +1,19 @@
-// Copyright 2009 Jack Christopher Kastorff
+/*
+ * Copyright (c) 2009 Chris Kastorff
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions, and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name Chris Kastorff may not be used to endorse or promote
+ *       products derived from this software without specific prior written
+ *       permission.
+ */
 
 #ifndef _DIE_H_
 #define _DIE_H_
